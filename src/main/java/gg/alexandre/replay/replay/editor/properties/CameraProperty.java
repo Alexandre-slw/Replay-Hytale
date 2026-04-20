529489cbd05d657ca8deaea12489b15741d7fb82
Alexandre <42965250+Alexandre-slw@users.noreply.github.com>
2026-04-20 18:36:47 +0200

    fix: camera interpolation

--- src/main/java/gg/alexandre/replay/replay/editor/properties/CameraProperty.java
+++ src/main/java/gg/alexandre/replay/replay/editor/properties/CameraProperty.java
@@ -52,16 +52,55 @@ public class CameraProperty extends BaseProperty<Position> {
 
             double ratio = (double) (tick - previousTick) / (nextTick - previousTick);
 
+            double p0Yaw = unwrapRelative(p0.yaw(), p1.yaw());
+            double p1Yaw = p1.yaw();
+            double p2Yaw = unwrapRelative(p2.yaw(), p1Yaw);
+            double p3Yaw = unwrapRelative(p3.yaw(), p2Yaw);
+
+            double p0Pitch = unwrapRelative(p0.pitch(), p1.pitch());
+            double p1Pitch = p1.pitch();
+            double p2Pitch = unwrapRelative(p2.pitch(), p1Pitch);
+            double p3Pitch = unwrapRelative(p3.pitch(), p2Pitch);
+
             double x = InterpolationUtil.catmullRom(p0.x(), p1.x(), p2.x(), p3.x(), ratio);
             double y = InterpolationUtil.catmullRom(p0.y(), p1.y(), p2.y(), p3.y(), ratio);
             double z = InterpolationUtil.catmullRom(p0.z(), p1.z(), p2.z(), p3.z(), ratio);
-            double yaw = InterpolationUtil.catmullRom(p0.yaw(), p1.yaw(), p2.yaw(), p3.yaw(), ratio);
-            double pitch = InterpolationUtil.catmullRom(p0.pitch(), p1.pitch(), p2.pitch(), p3.pitch(), ratio);
+
+            double yaw = InterpolationUtil.catmullRom(p0Yaw, p1Yaw, p2Yaw, p3Yaw, ratio);
+            double pitch = InterpolationUtil.catmullRom(p0Pitch, p1Pitch, p2Pitch, p3Pitch, ratio);
+
+            yaw = normalizeAngle(yaw);
+            pitch = normalizeAngle(pitch);
 
             return new Position(x, y, z, yaw, pitch);
         }
     }
 
+    private double unwrapRelative(double angle, double reference) {
+        double diff = angle - reference;
+
+        while (diff > Math.PI) {
+            angle -= 2.0 * Math.PI;
+            diff -= 2.0 * Math.PI;
+        }
+
+        while (diff < -Math.PI) {
+            angle += 2.0 * Math.PI;
+            diff += 2.0 * Math.PI;
+        }
+
+        return angle;
+    }
+
+    private double normalizeAngle(double angle) {
+        while (angle <= -Math.PI) {
+            angle += 2.0 * Math.PI;
+        }
+        while (angle > Math.PI) {
+            angle -= 2.0 * Math.PI;
+        }
+        return angle;
+    }
     @Override
     public void editKeyframe(@Nonnull ReplayState state, @Nonnull Player player,
                              @Nonnull UIEventContext<?> context, int tick) {
