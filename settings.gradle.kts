d5d65c4100360da1576e834093caa17b958d4bfd
Alexandre <42965250+Alexandre-slw@users.noreply.github.com>
2026-04-25 08:56:09 +0200

    chore: remove duplicated manifest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,3 @@ rootProject.name = "replay-plugin"
 plugins {
     id("dev.scaffoldit") version "0.2.+"
 }
-
-hytale {
-    usePatchline("release")
-    useVersion("latest")
-
-    repositories {
-
-    }
-
-    dependencies {
-
-    }
-
-    manifest {
-        Group = "Alexandre"
-        Name = "Replay"
-        Main = "gg.alexandre.replay.ReplayPlugin"
-        IncludesAssetPack = true
-    }
-}
